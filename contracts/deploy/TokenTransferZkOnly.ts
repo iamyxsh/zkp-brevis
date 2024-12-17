d6ed264d54d4a200c760b7e6a829cebd5af654d0
Yash Sharma <iamyxsh@gmail.com>
2024-12-17 12:35:00 +0530

    update deployment script

--- contracts/deploy/TokenTransferZkOnly.ts
+++ contracts/deploy/TokenTransferZkOnly.ts
@@ -1,17 +1,22 @@
 import * as dotenv from 'dotenv';
+import { ethers } from 'ethers';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { ethers } from 'hardhat';
 
 dotenv.config();
+
+const BREIS_REQUEST_SEPOLIA = '0x4a97B63b27576d774b6BD288Fa6aAe24F086B84c';
+const MEV_STAKE = '0xD222b11158932fb4E3c8A02214E2c5E7a0A996CA';
+const REWARD = ethers.parseEther('250');
+const VKHASH = '0x06138691d11a5d1111e2b83963ff143620d3db948ff205c5eaaeeb816e830d57';
 
 const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const args: string[] = ['0xa082F86d9d1660C29cf3f962A31d7D20E367154F', '', ethers.parseEther('10').toString()];
-  const deployment = await deploy('TokenTransferZkOnly', {
+  const args: any[] = [BREIS_REQUEST_SEPOLIA, MEV_STAKE, REWARD, VKHASH];
+  const deployment = await deploy('DextrZK', {
     from: deployer,
     log: true,
     args: args
